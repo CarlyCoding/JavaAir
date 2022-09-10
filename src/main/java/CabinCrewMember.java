62f73a20a136ab18ed8b840fc1cabdef34dfea22
CarlyCoding <carly_off@outlook.com>
2022-09-10 17:34:59 +0100

    Added pilot class

--- src/main/java/CabinCrewMember.java
+++ src/main/java/CabinCrewMember.java
@@ -1,2 +1,29 @@
 public enum CabinCrewMember {
+
+    FLIGHT_ATTENDANT("Kelly", "The menu choice today is chicken or beef"),
+
+    PURSER("Erin", "Cabin Crew seats for landing"),
+
+    FIRST_OFFICER("Dwight", "Please stay seated while the seatbelt sign is on!"),
+
+    CAPTAIN("Pamela", "We will be landing at Wilkes- Barre International in approx 10 minutes");
+
+    private String name;
+    private String announcement;
+
+    CabinCrewMember(String name, String announcement){
+        this.name = name;
+        this.announcement = announcement;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getAnnouncement(){
+        return announcement;
+    }
+
+
+
 }
--- src/main/java/Pilot.java
+++ src/main/java/Pilot.java
@@ -1,2 +1,31 @@
-public class Pilot {
+public class Pilot{
+
+    private String name;
+    private String rank;
+    private String licenseNumber;
+
+    private String flyPlane;
+
+    public Pilot(String name, String rank, String licenseNumber, String flyPlane){
+        this.name = name;
+        this.rank = rank;
+        this.licenseNumber = licenseNumber;
+        this.flyPlane = flyPlane;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getRank(){
+        return rank;
+    }
+
+    public String getLicenseNumber(){
+        return licenseNumber;
+    }
+
+    public String getFlyPlane(){
+        return flyPlane;
+    }
 }
