8851aa69d2f655234aca18561735d9d6204a22eb
CarlyCoding <carly_off@outlook.com>
2022-09-11 13:33:16 +0100

    Set up Flight class and Flight_test

--- src/main/java/Flight.java
+++ src/main/java/Flight.java
@@ -1,15 +1,46 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Flight {
 
-//    List pilot
-//    List CabinCrew
-//    List of passengers
+    private ArrayList<Passenger> passengers;
+    private ArrayList<CabinCrewMember> cabinCrewMembers;
+    private ArrayList<Pilot> pilots;
     private Plane plane;
     private String flightNumber;
     private String destination;
     private String departureAirport;
     private String departureTime;
+
+    public Flight(Plane plane, String flightNumber, String destination, String departureAirport, String departureTime){
+        this.passengers = new ArrayList<Passenger>();
+        this.cabinCrewMembers = new ArrayList<CabinCrewMember>();
+        this.pilots = new ArrayList<Pilot>();
+        this.plane = plane;
+        this.flightNumber = flightNumber;
+        this.destination = destination;
+        this.departureAirport = departureAirport;
+        this.departureTime = departureTime;
+    }
+
+    public String flightNumber(){
+        return flightNumber;
+    }
+
+    public String destination(){
+        return destination;
+    }
+
+    public String departureAirport(){
+        return departureAirport;
+    }
+
+    public String departureTime(){
+        return departureTime;
+    }
+
+
+
 
 
 }
--- src/test/java/Flight_Test.java
+++ src/test/java/Flight_Test.java
@@ -1,2 +1,11 @@
+import org.junit.Before;
+
 public class Flight_Test {
+
+    Flight flight;
+
+    @Before
+    public void setUp(){
+
+    }
 }
