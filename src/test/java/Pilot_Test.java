be2eb9ae19e69bfcc930279adc93aed5e865eb28
CarlyCoding <carly_off@outlook.com>
2022-09-11 12:19:00 +0100

    Pilot testing written- getName requires re-test.

--- src/test/java/Pilot_Test.java
+++ src/test/java/Pilot_Test.java
@@ -5,4 +5,35 @@ import static org.junit.Assert.assertEquals;
 
 public class Pilot_Test {
 
+    Pilot pilot;
+
+    @Before
+    public void before(){
+        pilot = new Pilot("Gerard",Rank.CAPTAIN, "LNR456","Cabin crew seats for take off");
+    }
+
+//    Write tests for get name.
+
+//    @Test
+//    public void getName(){
+//        assertEquals("Gerard", pilot.getName());
+//    }
+
+    @Test
+    public void hasRank(){
+        assertEquals(Rank.CAPTAIN, pilot.getRank());
+    }
+
+    @Test
+    public void hasLicenseNumber(){
+        assertEquals("LNR456", pilot.getLicenseNumber());
+    }
+
+    @Test
+    public void hasFlyPlane(){
+        assertEquals("Cabin crew seats for take off", pilot.getFlyPlane());
+    }
+
+
+
 }
